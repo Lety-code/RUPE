d3ea6961df51d9ee2d08b187da5daf8def1bd57d
Lety-code <infdmin@gmail.com>
2026-07-19 23:48:59 -0600

    Delete src/main directory

--- src/main/java/mx/unadm/rupe/repository/BitacoraRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.unadm.rupe.repository;
-
-import mx.unadm.rupe.model.Bitacora;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/* Repositorio para bitácora. */
-public interface BitacoraRepository extends JpaRepository<Bitacora, Long> {
-}
