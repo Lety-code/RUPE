72804a4cef4502306cec67e877220b2f53c248d5
Lety-code <infdmin@gmail.com>
2026-08-04 19:41:30 -0600

    Delete src/main directory

--- src/main/java/mx/unadm/rupe/repository/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.unadm.rupe.repository;
-
-import mx.unadm.rupe.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/* Repositorio para roles. */
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
